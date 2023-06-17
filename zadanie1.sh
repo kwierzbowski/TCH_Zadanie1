d1402c5e4aa53a7a02421c96146200ddf9f90afc
kwierzbowski <83925877+kwierzbowski@users.noreply.github.com>
2023-06-18 00:43:07 +0200

    Update zadanie1.sh

--- zadanie1.sh
+++ zadanie1.sh
@@ -1,7 +1,7 @@
 sudo docker build -t 95605/95605:1.0 --no-cache . \
 && sudo docker run -itd -p 3000:3000 --name 95605 95605/95605:1.0"" \
 && docker image inspect 95605/95605:1.0 | jq '.[].RootFS.Layers' \
-&& echo "go to http://localhost:3000/" \
+&& echo "http://localhost:3000/" \
 && docker scout cves 95605/95605:1.0 \
 && echo "Logi:" \
 && docker exec -it 95605 cat logs/logs.log
